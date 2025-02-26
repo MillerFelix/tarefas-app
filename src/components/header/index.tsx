2700de7c68cf80ca318902b5d520c40ba826ce71
MillerFelix <millerredigolo@gmail.com>
2025-02-26 15:00:03 -0300

    Implementando lógica de autenticação

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import styles from "./styles.module.css";
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Header() {
+  const { data: session, status } = useSession();
+  console.log(status);
   return (
     <header className={styles.cabecalho}>
       <section className={styles.conteudo}>
@@ -11,11 +14,26 @@ export default function Header() {
               Tarefas<span>+</span>
             </h1>
           </Link>
-          <Link href="/dashboard" className={styles.link}>
-            Meu Painel
-          </Link>
+          {session?.user && (
+            <Link href="/dashboard" className={styles.link}>
+              Meu Painel
+            </Link>
+          )}
         </nav>
-        <button className={styles.botaoLogin}>Acessar</button>
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button className={styles.botaoLogin} onClick={() => signOut()}>
+            Olá, {session?.user?.name}
+          </button>
+        ) : (
+          <button
+            className={styles.botaoLogin}
+            onClick={() => signIn("google")}
+          >
+            Acessar
+          </button>
+        )}
       </section>
     </header>
   );
